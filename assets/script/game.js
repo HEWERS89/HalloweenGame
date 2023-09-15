451adeaee026234c2cd824e721ef45ba8fd102ed
Hayley Ewers <hayleyewers@hotmail.co.uk>
2023-09-15 12:31:12 +0000

    added style to correct and wrong answers

---
+++ assets/script/game.js
@@ -0,0 +1,29 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click',startGame)
+
+function startGame() {
+    console.log('Game Started')
+}
+
+
+
+//Get players name//
+function playerName(){
+
+}
+
+
+function score(){
+
+}
+
+
+
+function NextQuestion(){
+}
+
+
+function selectAnswer(){
+
+}
