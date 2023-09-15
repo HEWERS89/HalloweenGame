33a8ef74b9fd314ad6efcc9029f1891e33dd6a7b
Hayley Ewers <hayleyewers@hotmail.co.uk>
2023-09-15 14:08:16 +0000

    Change game.js

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,9 +1,21 @@
-const startButton = document.getElementById('start-btn')
+const startButton = document.getElementById ('start-btn')
+const questionAreaElement = document.getElementById ('question-area')
+
+let 
+shuffledQuestions,
+currentQuestionIndex
+
+
+
 
 startButton.addEventListener('click',startGame)
 
+//hide start button initiate game|questions//
 function startGame() {
     console.log('Game Started')
+    startButton.classList.add('hide'),
+    questionAreaElement.classList.remove('hide'),
+    NextQuestion()
 }
 
 
@@ -25,5 +37,16 @@ function NextQuestion(){
 
 
 function selectAnswer(){
-
 }
+
+
+
+const questions = [
+    {
+        question: 'what is 1+1',
+        answers: [
+            {text: '4', correct:true},
+            {text: '22', correct:false},
+        ]
+    }
+]
