a204cd05c8497ba7ef8c942e7b3e11ed5b6bff23
Hayley Ewers <hayleyewers@hotmail.co.uk>
2023-09-15 14:36:39 +0000

    Move game functionality into index.html

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,20 +1,27 @@
-const startButton = document.getElementById ('start-btn')
-const questionAreaElement = document.getElementById ('question-area')
+const startButton = document.getElementById('start-btn')
+const questionAreaElement = document.getElementById('question-area')
+const loginContainer=document.getElementById("login-continer")
+const gameContainer=document.getElementById("game-container")
+const submitForm = document.getElementById("login-box")
+
 
 let 
 shuffledQuestions,
-currentQuestionIndex
+currentQuestionIndex,
+userName
 
 
 
-
-startButton.addEventListener('click',startGame)
+submitForm.addEventListener('submit', startGame)
+// startButton.addEventListener('click', startGame)
 
 //hide start button initiate game|questions//
 function startGame() {
     console.log('Game Started')
-    startButton.classList.add('hide'),
-    questionAreaElement.classList.remove('hide'),
+    // startButton.classList.add('hide'),
+    // questionAreaElement.classList.remove('hide'),
+    loginContainer.classList.add("hide")
+    gameContainer.classList.remove("hide")
     NextQuestion()
 }
 
